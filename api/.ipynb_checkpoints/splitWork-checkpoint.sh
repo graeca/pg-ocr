f27df481ce4fad26a798e1191db57b0805688d78
evagelosvar <evagelosvar@gmail.com>
2019-05-03 13:37:50 +0000

    update

--- api/.ipynb_checkpoints/splitWork-checkpoint.sh
+++ api/.ipynb_checkpoints/splitWork-checkpoint.sh
@@ -1,3 +1,5 @@
+rm -r fragments
+mkdir fragments
 workFile='pg/author1/work.txt'
 workFile="$1"
-split  --additional-suffix=".frg" -a 5 -d -l 20 "$workFile" fragments/fragment-$id- #fragmnets
+split  --additional-suffix=".frg" -a 5 -d -l 10 "$workFile" fragments/fragment-$id- #fragmnets
