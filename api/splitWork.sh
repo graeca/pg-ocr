2b835ead8c7ad56723c1a42719078784cbc38fe2
evagelosvar <evagelosvar@gmail.com>
2019-05-05 19:15:39 +0000

    update

--- api/splitWork.sh
+++ api/splitWork.sh
@@ -2,4 +2,7 @@ rm -r fragments
 mkdir fragments
 #workFile='pg/author1/work.txt'
 workFile="$1"
-split  --additional-suffix=".frg" -a 5 -d -l 10 "$workFile" fragments/fragment-$id- #fragmnets
+authorId="$2"
+workId="$3"
+split  --additional-suffix=".frg" -a 5 -d -l 10 "$workFile" fragments/fragment-"$authorId"-"$workId"- 
+#fragmnets
