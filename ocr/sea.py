ba73b32e6db38b630376b0294fa2d0e468d9dc28
Your Name <you@example.com>
2019-06-13 13:27:35 +0200

    update

--- ocr/sea.py
+++ ocr/sea.py
@@ -94,11 +94,17 @@ def compareHUandSC(queryFile, dataFile):
 
 
 wordtokens = get_ipython().getoutput("find word -type f -name '*.png'| sed -n /ipynb/!p")
-wordtokens= ["word/pneuma.png","word/kyrios.png","word/xristou.png","word/patera.png"]
 
-wordtokens= ['word/pantokrator.png','word/apavgazmatos.png','word/monogeni.png','word/agenitos.png']
-wordtokens= ['word/pantokratoros.png','word/korinthiois.png','word/exomologisetai.png','word/akatalhptos.png']
-wordtokens= ["word/Ihsou.png","word/paulos.png","word/ypostateos.png","word/theos.png","word/oti.png","word/gar.png"]
+
+#wordtokens= ['word/pantokratoros.png','word/korinthiois.png','word/exomologisetai.png','word/akatalhptos.png']#ok
+#wordtokens= ['word/pantokrator.png','word/apavgazmatos.png','word/monogeni.png','word/agenitos.png']#ok
+#wordtokens= ["word/Ihsou.png","word/paulos.png","word/ypostateos.png","word/theos.png","word/oti.png","word/gar.png"]
+
+#wordtokens= ["word/pneuma.png","word/kyrios.png","word/xristou.png","word/patera.png"]
+
+#wordtokens= ["word/dauid.png"]
+wordtokens= ["word/agion.png"]
+
 for queryFile in wordtokens:
     print(queryFile)
     
